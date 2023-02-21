a0616b382df7acf1928d6b471c01ac06aac32206
Thomas Alsop <2015861@csl-student.wlv.ac.uk>
2023-02-21 15:08:54 +0000

    Added API to the jobs page

--- Views/pages/home.php
+++ Views/pages/home.php
@@ -3,8 +3,29 @@
 <body>
 
 <?php
-echo "This is the Home Page!";
-?>
 
+// Set connection details
+$login = 'deb7974b-5a07-410d-a047-8998af8482df';
+$password = '';
+$url = 'https://www.reed.co.uk/api/1.0/search?keywords=laravel&location=wolverhampton&distancefromlocation=15';
+
+// Create CURL object with options
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
+
+// Make CURL call, and convert result to JSON object
+$jobs = curl_exec($ch);
+$jobs = json_decode($jobs);
+curl_close($ch);  
+
+// Display results
+foreach($jobs->results as $job) {
+	print($job->jobTitle . "<br>");
+}
+
+?>
 </body>
 </html>
