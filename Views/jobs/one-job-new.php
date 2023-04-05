980afdbad3527dc414a4aa16805f6c48f9717abd
Thomas Alsop <2015861@csl-student.wlv.ac.uk>
2023-04-05 20:14:13 +0100

    creating a container to hold the data from a particular job ID

--- Views/jobs/one-job-new.php
+++ Views/jobs/one-job-new.php
@@ -1,17 +1,6 @@
 <?php
 
 
-
-
-
-
-
-
-
-
-
-
-
 $imgs = array(
 	"https://cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png",
 	"https://profilemagazine.com/wp-content/uploads/2020/04/Ajmere-Dale-Square-thumbnail.jpg",
@@ -58,6 +47,23 @@ $imgs = array(
 		<br />
 		  <div class="container" >
 			<!-- Content for first nested container goes here -->
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 				
